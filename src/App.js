d3b1e301f6cbf27915dbaee208e7a461657f694c
jriccrussel <jriccrussel@gmail.com>
2021-10-30 01:06:38 +0800

    Adding Lights

--- src/App.js
+++ src/App.js
@@ -18,11 +18,29 @@ import { useInView } from "react-intersection-observer"
 
 function Model() {
   const gltf = useGLTF("/armchairYellow.gltf", true)
-
   return <primitive object={gltf.scene} dispose={null} />
 }
 
+// Lights
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={0.3}  />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight
+        position={[0, 10, 0]}
+        intensity={1.5}
+      />
+      <spotLight intensity={1} position={[1000, 0, 0]} />
+    </>
+  )
+} 
+
+// Model & Content
 const HTMLContent = () => {
+  const ref = useRef()
+
+
   return (
     <Section factor={1.5} offset={1}>
       <group position={[0, 250, 0]}>
@@ -48,7 +66,7 @@ export default function App() {
         colorManagement
         camera={{ position: [0, 0, 120], fov: 70 }}>
         {/* Lights Component */}
-
+        <Lights />
         <Suspense fallback={null}>
           <HTMLContent />
         </Suspense>
